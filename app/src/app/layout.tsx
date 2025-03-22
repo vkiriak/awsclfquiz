1052d22679cc986aed10ae9a6ff04b9c08b2fe20
vkiriak <chiriac.v.c@gmail.com>
2025-03-21 21:08:48 -0400

    fix layout head bug

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
